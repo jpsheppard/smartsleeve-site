a5668e0317c29231f1eaebc77141822d5163aee9
jpsheppard <jpsheppard88@gmail.com>
2026-07-10 15:38:29 -0700

    Deploy approved SS mockup cache revision

--- app/merch.js
+++ app/merch.js
@@ -5,7 +5,7 @@
   var catalogEndpoint = meta("smartsleeve-merch-catalog-endpoint");
   var authEndpoint = meta("smartsleeve-auth-endpoint");
   var registerEndpoint = authEndpoint ? authEndpoint.replace(/\/$/, "") + "/register" : "";
-  var merchImageVersion = "20260710-approved-ss-reference-v2";
+  var merchImageVersion = "20260710-approved-ss-reference-v3";
   var staticCatalogEndpoint = "/merch/printful-storefront-catalog.json";
   var state = {
     products: [],
